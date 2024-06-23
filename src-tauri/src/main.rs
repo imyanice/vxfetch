672bf522c7fa86f1dc41a019f778e292732755be
Yanice <git@yanice.me>
2024-06-23 13:28:18 +0200

    feat: added the download files buttons + interaction

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::Write;
+use std::path::Path;
+use tauri::{AppHandle, Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,7 +14,35 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, download_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn download_files(app_handle: tauri::AppHandle, topic: String, encoded_topic: String) {
+    //topic needs to be already encoded in uri format from js layer
+    let request = reqwest::get(format!("http://localhost:3000/download?topic={}", encoded_topic)).await.unwrap();
+    let buffer = request.bytes().await.unwrap();
+    let mut file = std::fs::File::create(format!("{}{}.zip", get_storage_folder(app_handle), topic)).unwrap();
+    let _ = file.write(&*buffer);
+}
+
+fn get_storage_folder(app_handle: AppHandle) -> String {
+    let storage_path = format!("{}/.vxfetch/", app_handle.path().home_dir().unwrap().to_str().unwrap().to_string());
+    match Path::new(storage_path.clone().as_str()).try_exists() {
+        Ok(r) => {
+            if r {
+                storage_path
+            } else {
+                std::fs::create_dir(storage_path.clone()).expect("TODO: panic message");
+                storage_path
+            }
+        }
+        Err(err) => {println!("{}", err);
+            "".parse().unwrap()
+        }
+    }
+
+
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
 import libraryImage from "./assets/libraryImage.png"
+import { ChangeEvent, useState } from 'react'
+import { invoke } from '@tauri-apps/api/core'
 
 function App() {
 
-	return (
+    const [completions, setCompletions] = useState([])
+
+    function onChange(e: ChangeEvent<HTMLInputElement>) {
+        let value = e.target.value
+        if (value.length < 3) {
+            setCompletions([])
+            return
+        }
+        fetch("http://localhost:3000/search/?topic=" + value).then((res) => res.json().then((json) => {
+            if (!json.success) return
+            setCompletions(json.matches)
+        }))
+        // console.log(e.target.value)
+    }
+
+    function download(index: number) {
+        invoke("greet", {name: ""}).then(c => console.log(c))
+        //@ts-ignore
+        invoke("download_files", {topic: completions[index].item.topic, encodedTopic: encodeURI(completions[index].item.topic)})
+    }
+
+    return (
         <>
             <div className="flex w-44 flex-col fixed inset-y-0">
                 <div className="flex-1 flex flex-col min-h-0 bg-[#222] border-2 border-[#1E1E1E]">
@@ -17,8 +40,17 @@ function App() {
 
             <div className="w-full text-neutral-300">
                 <div className={ 'p-4 px-64' }>
-                    <input type={ 'text' } className={ 'bg-[#222] w-full text-xl p-3 rounded focus:outline-none border-2 border-[#1E1E1E]' }
-                           placeholder={ 'search vx-underground' }/>
+                    <input onChange={onChange} type={ 'text' } className={ 'bg-[#222] w-full text-xl p-3 rounded-md focus:outline-none border-2 border-[#1E1E1E]' }
+                           placeholder={ 'search vx-underground' } />
+                    { completions.length > 1 ?
+                    <div className={ "bg-[#222] w-full text-sm p-3 mt-2 rounded-md focus:outline-none border-2 border-[#1E1E1E]" }>
+                        {completions.map((completion, index) => (
+                        // @ts-ignore
+                        <div className={"cursor-pointer"} onClick={() => download(index)}>{completion.item.topic}</div>
+                    ))}
+                    </div> : <></>
+                }
+
                 </div>
             </div>
         </>
